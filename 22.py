d579237b1808cb38b2fa794d3e5552e7323ebacd
msy <m.yamakawa.cubic@gmail.com>
2016-11-03 00:17:01 +0900

    22 answer

--- 22.py
+++ 22.py
@@ -2,6 +2,6 @@ import re
 
 with open('data/jawiki-uk.txt', 'r') as f:
     for line in f.readlines():
-        data = re.search("Category:(.*)", line)
+        data = re.search("Category:(.*)\]{2}", line)
         if data is not None:
             print(data.group(1))
