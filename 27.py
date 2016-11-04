977930d07bc6f4b4cd3c6320500bc53925fdf6bb
msy <m.yamakawa.cubic@gmail.com>
2016-11-05 03:43:30 +0900

    27 answer

---
+++ 27.py
@@ -0,0 +1,17 @@
+import re
+
+dict = {}
+
+with open('data/jawiki-uk.txt') as f:
+    for line in f.readlines():
+        data = re.search("\|(?P<key>\S*)\s=\s(?P<object>\S*)",line)
+        if data is not None:
+            key = data.group('key')
+            keyTF = re.search("('{2,5}(.*)'{2,5})|(\[\[[^ファイル](.*)\]\])",data.group('key'))
+            if keyTF is not None:
+                key = re.sub(r"('+)|(\[+)|(\]+)", "", data.group('key'))
+            object = data.group('object')
+            objectTF = re.search("('{2,5}(.*)'{2,5})|(\[\[[^ファイル](.*)\]\])",data.group('object'))
+            if objectTF is not None:
+                object = re.sub(r"('+)|(\[+)|(\]+)","",data.group('object'))
+            dict[key] = object
