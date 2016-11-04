ffc9457d0560b649c876538875826eb20d9219e3
msy <m.yamakawa.cubic@gmail.com>
2016-11-05 03:36:42 +0900

    26 answer

--- 26.py
+++ 26.py
@@ -6,12 +6,13 @@ with open('data/jawiki-uk.txt') as f:
     for line in f.readlines():
         data = re.search("\|(?P<key>\S*)\s=\s(?P<object>\S*)",line)
         if data is not None:
-            key = re.findall("'{2,5}(.*)'{2,5}",data.group('key'))
-            if key is not None:
+            key = data.group('key')
+            keyTF = re.search("'{2,5}(.*)'{2,5}",data.group('key'))
+            if keyTF is not None:
                 key = re.sub(r"'+", "", data.group('key'))
-
-            object = re.findall("'{2,5}(.*)'{2,5}",data.group('object'))
-            if object is not None:
+            object = data.group('object')
+            objectTF = re.search("'{2,5}(.*)'{2,5}",data.group('object'))
+            if objectTF is not None:
                 object = re.sub(r"'","",data.group('object'))
             dict[key] = object
-print(dict)
+            print(object)
